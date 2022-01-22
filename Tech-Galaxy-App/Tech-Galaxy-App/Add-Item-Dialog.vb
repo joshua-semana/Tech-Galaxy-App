34a8b9f362291753308a0a8cb9ed446e9ca76b55
leonardoroen <97822573+leonardoroen@users.noreply.github.com>
2022-01-22 20:12:06 +0800

    Update Add-Item-Dialog.vb

--- Tech-Galaxy-App/Tech-Galaxy-App/Add-Item-Dialog.vb
+++ Tech-Galaxy-App/Tech-Galaxy-App/Add-Item-Dialog.vb
@@ -1,15 +1,25 @@
 ﻿Imports System.Windows.Forms
-
+Imports System.Data.OleDb
 Public Class dlgAddItem
 
     Public Sub ClearData()
         txtItemName.Text = ""
+        cmbCategory.Text = ""
         txtPrice.Text = ""
         numStock.Value = 1
     End Sub
 
     Private Sub btnAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAdd.Click
         ClearData()
+        Using cmd As New OleDbCommand("INSERT INTO tbl_exampleitems ([item_name],[category],[price],[stock]) VALUES('" & txtItemName.Text & "','" & cmbCategory.Text & "','" & txtPrice.Text & "','" & numStock.Value & "')", con)
+            If txtItemName.Text = Nothing And cmbCategory.Text = Nothing And txtPrice.Text = Nothing And numStock.Value = 0 Then
+                MsgBox("Please fill the required Information", MsgBoxStyle.Exclamation)
+                Exit Sub
+            End If
+            MsgBox("Data has been inserted successfully", MsgBoxStyle.Information)
+            cmd.ExecuteNonQuery()
+        End Using
+
         Me.Close()
     End Sub
 
@@ -23,5 +33,9 @@ Public Class dlgAddItem
             e.Handled = True
             MsgBox("Please Enter Number Only", vbCritical)
         End If
+    End Sub
+
+    Private Sub dlgAddItem_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ClearData()
     End Sub
 End Class
