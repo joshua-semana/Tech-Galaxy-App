407a8b08e1e757a01d97c5c7a3797636fa71d62b
leonardoroen <97822573+leonardoroen@users.noreply.github.com>
2022-02-06 00:22:54 +0800

    Update Module1.vb

--- Tech-Galaxy-App/Tech-Galaxy-App/Module1.vb
+++ Tech-Galaxy-App/Tech-Galaxy-App/Module1.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Data.OleDb
 Module Module1
     Public con As OleDbConnection = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\db_techgalaxy.accdb")
+    Public StoreDate As String
 End Module
